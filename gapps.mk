e16b80be6e7281e822d90d5bc8b84d815bbcf2c2
Codename Android <codenamedroid@codenameandroid.com>
2012-03-31 16:58:52 -0500

    new path for google apps

--- gapps.mk
+++ gapps.mk
@@ -1,63 +1,63 @@
 
 #app folder
 PRODUCT_COPY_FILES += \
-    vendor/google/apps/system/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
-    vendor/google/apps/system/app/GalleryGoogle.apk:system/app/GalleryGoogle.apk \
-    vendor/google/apps/system/app/GenieWidget.apk:system/app/GenieWidget.apk \
-    vendor/google/apps/system/app/Gmail.apk:system/app/Gmail.apk \
-    vendor/google/apps/system/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
-    vendor/google/apps/system/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
-    vendor/google/apps/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
-    vendor/google/apps/system/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
-    vendor/google/apps/system/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
-    vendor/google/apps/system/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
-    vendor/google/apps/system/app/GoogleQuickSearchBox.apk:system/app/GoogleQuickSearchBox.apk \
-    vendor/google/apps/system/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
-    vendor/google/apps/system/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
-    vendor/google/apps/system/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
-    vendor/google/apps/system/app/MediaUploader.apk:system/app/MediaUploader.apk \
-    vendor/google/apps/system/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
-    vendor/google/apps/system/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
-    vendor/google/apps/system/app/SetupWizard.apk:system/app/SetupWizard.apk \
-    vendor/google/apps/system/app/Talk.apk:system/app/Talk.apk \
-    vendor/google/apps/system/app/Vending.apk:system/app/Vending.apk \
-    vendor/google/apps/system/app/VoiceSearch.apk:system/app/VoiceSearch.apk
+    vendor/google/system/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
+    vendor/google/system/app/GalleryGoogle.apk:system/app/GalleryGoogle.apk \
+    vendor/google/system/app/GenieWidget.apk:system/app/GenieWidget.apk \
+    vendor/google/system/app/Gmail.apk:system/app/Gmail.apk \
+    vendor/google/system/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+    vendor/google/system/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+    vendor/google/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+    vendor/google/system/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
+    vendor/google/system/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+    vendor/google/system/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
+    vendor/google/system/app/GoogleQuickSearchBox.apk:system/app/GoogleQuickSearchBox.apk \
+    vendor/google/system/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+    vendor/google/system/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
+    vendor/google/system/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
+    vendor/google/system/app/MediaUploader.apk:system/app/MediaUploader.apk \
+    vendor/google/system/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
+    vendor/google/system/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
+    vendor/google/system/app/SetupWizard.apk:system/app/SetupWizard.apk \
+    vendor/google/system/app/Talk.apk:system/app/Talk.apk \
+    vendor/google/system/app/Vending.apk:system/app/Vending.apk \
+    vendor/google/system/app/VoiceSearch.apk:system/app/VoiceSearch.apk
 
 #etc/permissions
 PRODUCT_COPY_FILES += \
-    vendor/google/apps/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
-    vendor/google/apps/system/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
-    vendor/google/apps/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
-    vendor/google/apps/system/etc/permissions/features.xml:system/etc/permissions/features.xml
+    vendor/google/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    vendor/google/system/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    vendor/google/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
+    vendor/google/system/etc/permissions/features.xml:system/etc/permissions/features.xml
 
 #framework
 PRODUCT_COPY_FILES += \
-    vendor/google/apps/system/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
-    vendor/google/apps/system/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
-    vendor/google/apps/system/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar
+    vendor/google/system/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
+    vendor/google/system/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+    vendor/google/system/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar
 
 #lib
 PRODUCT_COPY_FILES += \
-    vendor/google/apps/system/lib/libflint_engine_jni_api.so:system/lib/libflint_engine_jni_api.so \
-    vendor/google/apps/system/lib/libfrsdk.so:system/lib/libfrsdk.so \
-    vendor/google/apps/system/lib/libgcomm_jni.so:system/lib/libgcomm_jni.so \
-    vendor/google/apps/system/lib/libmono.so:system/lib/libmono.so \
-    vendor/google/apps/system/lib/libpicowrapper.so:system/lib/libpicowrapper.so \
-    vendor/google/apps/system/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
-    vendor/google/apps/system/lib/libunity.so:system/lib/libunity.so \
-    vendor/google/apps/system/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
-    vendor/google/apps/system/lib/libvideochat_stabilize.so:system/lib/libvideochat_stabilize.so \
-    vendor/google/apps/system/lib/libvoicesearch.so:system/lib/libvoicesearch.so
+    vendor/google/system/lib/libflint_engine_jni_api.so:system/lib/libflint_engine_jni_api.so \
+    vendor/google/system/lib/libfrsdk.so:system/lib/libfrsdk.so \
+    vendor/google/system/lib/libgcomm_jni.so:system/lib/libgcomm_jni.so \
+    vendor/google/system/lib/libmono.so:system/lib/libmono.so \
+    vendor/google/system/lib/libpicowrapper.so:system/lib/libpicowrapper.so \
+    vendor/google/system/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
+    vendor/google/system/lib/libunity.so:system/lib/libunity.so \
+    vendor/google/system/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
+    vendor/google/system/lib/libvideochat_stabilize.so:system/lib/libvideochat_stabilize.so \
+    vendor/google/system/lib/libvoicesearch.so:system/lib/libvoicesearch.so
 
 #tts
 PRODUCT_COPY_FILES += \
-    vendor/google/apps/system/tts/lang_pico/de-DE_gl0_sg.bin:system/tts/lang_pico/de-DE_gl0_sg.bin \
-    vendor/google/apps/system/tts/lang_pico/de-DE_ta.bin:system/tts/lang_pico/de-DE_ta.bin \
-    vendor/google/apps/system/tts/lang_pico/es-ES_ta.bin:system/tts/lang_pico/es-ES_ta.bin \
-    vendor/google/apps/system/tts/lang_pico/es-ES_zl0_sg.bin:system/tts/lang_pico/es-ES_zl0_sg.bin \
-    vendor/google/apps/system/tts/lang_pico/fr-FR_nk0_sg.bin:system/tts/lang_pico/fr-FR_nk0_sg.bin \
-    vendor/google/apps/system/tts/lang_pico/fr-FR_ta.bin:system/tts/lang_pico/fr-FR_ta.bin \
-    vendor/google/apps/system/tts/lang_pico/it-IT_cm0_sg.bin:system/tts/lang_pico/it-IT_cm0_sg.bin \
-    vendor/google/apps/system/tts/lang_pico/it-IT_ta.bin:system/tts/lang_pico/it-IT_ta.bin
+    vendor/google/system/tts/lang_pico/de-DE_gl0_sg.bin:system/tts/lang_pico/de-DE_gl0_sg.bin \
+    vendor/google/system/tts/lang_pico/de-DE_ta.bin:system/tts/lang_pico/de-DE_ta.bin \
+    vendor/google/system/tts/lang_pico/es-ES_ta.bin:system/tts/lang_pico/es-ES_ta.bin \
+    vendor/google/system/tts/lang_pico/es-ES_zl0_sg.bin:system/tts/lang_pico/es-ES_zl0_sg.bin \
+    vendor/google/system/tts/lang_pico/fr-FR_nk0_sg.bin:system/tts/lang_pico/fr-FR_nk0_sg.bin \
+    vendor/google/system/tts/lang_pico/fr-FR_ta.bin:system/tts/lang_pico/fr-FR_ta.bin \
+    vendor/google/system/tts/lang_pico/it-IT_cm0_sg.bin:system/tts/lang_pico/it-IT_cm0_sg.bin \
+    vendor/google/system/tts/lang_pico/it-IT_ta.bin:system/tts/lang_pico/it-IT_ta.bin
 
 
